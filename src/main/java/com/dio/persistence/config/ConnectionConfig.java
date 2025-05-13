c4bf600a8fb8c147f67bf83a97abda3ca79c075c
Pedro Augusto <pedroaugustogoncalvesribeiro@gmail.com>
2025-05-13 18:12:54 -0300

    Iniciando projeto de Board de Tarefas

---
+++ src/main/java/com/dio/persistence/config/ConnectionConfig.java
@@ -0,0 +1,9 @@
+package com.dio.persistence.config;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ConnectionConfig {
+    
+}
